c04ef31d23be5ca7bfd9605bb669261d327af9e5
Aleksandra <aleksandrazatirka21@gmail.com>
2025-01-24 16:41:33 -0500

    final

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -6,6 +6,12 @@ import { useDispatch, useSelector } from 'react-redux';
 import Cart from './CartItem';
 function ProductList() {
     const [showCart, setShowCart] = useState(false); 
+    const handleCart = () => {
+        setShowCart(true);
+    };
+    const handleProducts = () =>  {
+            setShowCart(false);
+        };
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
     const dispatch = useDispatch ();
